e320ad8ff4285896a255b8784a420de3a97f80c0
shubh-nih <shubhambisht149@gmail.com>
2025-11-05 17:06:14 +0530

    add

--- 02-Resume.py
+++ 02-Resume.py
@@ -1,23 +1,23 @@
 import streamlit as st
-import base64
+from streamlit_pdf_viewer import pdf_viewer
 st.markdown("<p style='text-align: top-right;'>Portfolio</p>", unsafe_allow_html=True)
 
 st.markdown("<h1 style='text-align: center;font-family: serif;'>RESUME</h1>", unsafe_allow_html=True)
 
 st.sidebar.markdown("# Resume")
 
-with open("shubham-ml-resume.pdf", "rb") as pdf_file:
-    pdf_bytes = pdf_file.read()
-    
-st.download_button(
-    label="📄 Download Resume",
-    data=pdf_bytes,
-    file_name="shubham-ml-resume.pdf",
-    mime="application/pdf"
-)
 
-# Display PDF using iframe
-st.markdown(
-    f'<iframe src="data:application/pdf;base64,{base64.b64encode(pdf_bytes).decode()}" width="700" height="800" type="application/pdf"></iframe>',
-    unsafe_allow_html=True
-)
+
+st.title("📄 Resume")
+
+# Display PDF
+pdf_viewer("shubham-ml-resume.pdf", width=700)
+
+# Add download button
+with open("shubham-ml-resume.pdf", "rb") as pdf_file:
+    st.download_button(
+        label="⬇️ Download Resume",
+        data=pdf_file,
+        file_name="shubham-ml-resume.pdf",
+        mime="application/pdf"
+    )

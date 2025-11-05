6add1aa86867107256a02b17b1b35c406f4c2743
shubh-nih <shubhambisht149@gmail.com>
2025-11-05 17:01:56 +0530

    add

--- 02-Resume.py
+++ 02-Resume.py
@@ -5,3 +5,5 @@ st.markdown("<p style='text-align: top-right;'>Portfolio</p>", unsafe_allow_html
 st.markdown("<h1 style='text-align: center;font-family: serif;'>RESUME</h1>", unsafe_allow_html=True)
 
 st.sidebar.markdown("# Resume")
+
+st.pdf("shubham-ml-resume.pdf", height=600)
